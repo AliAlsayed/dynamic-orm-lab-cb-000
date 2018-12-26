cbade5fe396116126d7c137889b65aa1bbd80f6f
Ali Alsayed <a_alsayed87@hotmail.com>
2018-12-26 11:17:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,27 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    table_info = DB[:conn].execute("PRAGMA table_info('#{table_name}')")
+    column_names = []
+    table_info.each do |h|
+      column_names << h["name"]
+    end
+    column_names.compact
+  end
+
+  self.column_names.each do |name|
+    attr_accessor name.to_sym
+  end
+
+  def initialize(options={})
+    options.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
 end
